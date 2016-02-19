3e5207588291cead961e087b885cfa2b9c2b19f0
Jessica <jessicaslaughter@utexas.edu>
2016-02-19 13:47:37 -0600

    lots of edits oops

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	$('a[href*="#"]').click(function(e) {
+		e.preventDefault();
+		var $target = $($(this).attr('href'));
+		var scrollTop = $target.offset().top;
+		$('html, body').animate({'scrollTop': scrollTop}, 500);
+	});
+
+});
